093298b088ac0d33b9abf2f8266371f2dffe6747
kamranahmad123 <kamrankhan.ahmad2@yahoo.com>
2023-03-10 01:09:17 -0800

    resolve problems

--- src/completionStatus.js
+++ src/completionStatus.js
@@ -1,13 +1,11 @@
-import { storage } from "./newFunctionalities";
-
 export default function checkDelete(checkEntry, tasksData) {
-  tasksData.forEach((toDoElements) => {
-    if (toDoElements.index === checkEntry.index && checkEntry.completed === true) {
-      toDoElements.completed = false;
-      storage(tasksData);
-    } else if (toDoElements.index === checkEntry.index && checkEntry.completed === false) {
-      toDoElements.completed = true;
-      storage(tasksData);
+  tasksData.forEach((toDoElement) => {
+    if (toDoElement.index === checkEntry.index && checkEntry.completed === true) {
+      toDoElement.completed = false;
+      localStorage.setItem('task', JSON.stringify(tasksData));
+    } else if (toDoElement.index === checkEntry.index && checkEntry.completed === false) {
+      toDoElement.completed = true;
+      localStorage.setItem('task', JSON.stringify(tasksData));
     }
   });
   window.location.reload();
